fbfcd8299a683ae30b3e2aee0d988ebffab1a5c4
Muskan <muskandn02@gmail.com>
2024-07-21 13:39:26 +0530

    search suggestions resolved

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -58,7 +58,7 @@ const SearchBar = () => {
   const handleFocus = () => {
     setIsFocused(true);
     if (query === "") {
-      setSuggestions(recentSearches);
+      setSuggestions(recentSearches.slice(0,5));
     }
   };
 
